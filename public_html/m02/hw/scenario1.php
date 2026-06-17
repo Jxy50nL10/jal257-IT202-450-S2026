ff06631357829626b0527f2fdd8f7404a630fdb0
Jxy50nL10 <jal257@njit.edu>
2026-06-17 18:16:58 -0400

    completed scenario 1

--- public_html/m02/hw/scenario1.php
+++ public_html/m02/hw/scenario1.php
@@ -35,10 +35,10 @@ function printOdds($arr, $arrayNumber)
    }
 
    }
- $output_result = implode(", ", $oddNumbers);
+ $output_result = implode(" ,", $oddNumbers);
     // End Solution Edits
     printScenario1Output($output_result);
-    echo "</div>";
+    echo "/div";
 }
 
 // Run the problem
